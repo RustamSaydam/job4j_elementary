ccae3bf7f363fa0741490fd0dff8731446fb3dba
RustamSaydam <rustamsaydam@icloud.com>
2022-04-22 13:02:54 +0300

    Редактирование

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -6,15 +6,15 @@ public class Calculator {
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
         System.out.println(onePlusTwo);
+        int six = 6;
+        int sixDivTwo = six / two;
         System.out.println(sixDivTwo);
+        int five = 5;
+        int fiveMinusTwo = five - two;
         System.out.println(fiveMinusTwo);
+        int four = 4;
+        int fourTimeTwo = four * two;
         System.out.println(fourTimeTwo);
-     }
+    }
 }
