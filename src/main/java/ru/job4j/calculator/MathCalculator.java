2af1238526c60980885d42d8e28940d88b4990f0
RustamSaydam <rustamsaydam@icloud.com>
2022-04-26 18:06:03 +0300

    ConverterTest

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+import ru.job4j.math.MathFunction.*;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double rustAndMisha(double first, double second) {
+        return rust(first, second) - misha(first, second);
+    }
+
+    public static double sasha(double first, double second) {
+        return sum(first, second) + multiply(first, second) + rust(first, second) + misha(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + rustAndMisha(10, 20));
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + sasha(10, 20));
+    }
+}
+
+
+
+
