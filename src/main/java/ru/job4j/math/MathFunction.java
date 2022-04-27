34d8ed4076f7fadfe3ca0014fd13b55006f86aa4
RustamSaydam <rustamsaydam@icloud.com>
2022-04-27 10:48:59 +0300

    Что такое import

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -1,11 +1,11 @@
 package ru.job4j.math;
 
-public class   MathFunction {
+public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
 
-    public static double ultiply(double first, double second) {
+    public static double multiply(double first, double second) {
 
         return first * second;
     }
@@ -18,5 +18,6 @@ public class   MathFunction {
         return first / second;
     }
 }
+
 
 
