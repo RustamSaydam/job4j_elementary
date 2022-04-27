25306f5099c4208f8278cc522a99cc1eb921b1c9
RustamSaydam <rustamsaydam@icloud.com>
2022-04-27 12:55:24 +0300

    MathCalculator

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -8,18 +8,18 @@ public class MathCalculator {
         return sum(first, second) + multiply(first, second);
     }
 
-    public static double rustAndMisha(double first, double second) {
-        return rust(first, second) - misha(first, second);
+    public static double roomAndArise(double first, double second) {
+        return room(first, second) - arise(first, second);
     }
 
-    public static double sasha(double first, double second) {
-        return sum(first, second) + multiply(first, second) + rust(first, second) + misha(first, second);
+    public static double cent(double first, double second) {
+        return sum(first, second) + multiply(first, second) + room(first, second) + arise(first, second);
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + rustAndMisha(10, 20));
+        System.out.println("Результат расчета равен: " + roomAndArise(10, 20));
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-        System.out.println("Результат расчета равен: " + sasha(10, 20));
+        System.out.println("Результат расчета равен: " + cent(10, 20));
     }
 }
 
