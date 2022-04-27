edb91c5acf8a1b4ab126065134dd56f63fa5a41b
RustamSaydam <rustamsaydam@icloud.com>
2022-04-27 10:47:52 +0300

    jb

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -5,7 +5,7 @@ import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second) + multiply(first, second);
+        return sum(first, second) + multiply(first, dsecond);
     }
 
     public static double rustAndMisha(double first, double second) {
