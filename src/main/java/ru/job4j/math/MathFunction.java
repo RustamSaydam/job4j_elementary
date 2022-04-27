1ac172050ea5cfeb5eb4981d994a2a27409fe6c7
RustamSaydam <rustamsaydam@icloud.com>
2022-04-27 12:52:52 +0300

    MathFunction

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -2,19 +2,19 @@ package ru.job4j.math;
 
 public class MathFunction {
     public static double sum(double first, double second) {
+
         return first + second;
     }
 
     public static double multiply(double first, double second) {
-
         return first * second;
     }
 
-    public static double rust(double first, double second) {
+    public static double room(double first, double second) {
         return first - second;
     }
 
-    public static double misha(double first, double second) {
+    public static double arise(double first, double second) {
         return first / second;
     }
 }
