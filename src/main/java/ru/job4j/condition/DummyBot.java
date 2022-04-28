5847f899429baec3e34eba6a1ba72d176519ccd2
RustamSaydam <rustamsaydam@icloud.com>
2022-04-28 16:39:37 +0300

    DummyBot

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,9 +1,10 @@
 package ru.job4j.condition;
 
 public class DummyBot {
+
     public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
-        if ("Привет, Бот.".equals(question)) {
+        if ("Привет Бот".equals(question)) {
             rsl = "Привет, умник.";
         } else if ("Пока.".equals(question)) {
             rsl = "До вскорой встречи.";
@@ -12,12 +13,13 @@ public class DummyBot {
     }
 
     public static void main(String[] args) {
-        String rsl = DummyBot.answer("Привет, Бот.");
+        String rsl = DummyBot.answer("Привет Бот");
         System.out.println(rsl);
         rsl = DummyBot.answer("Пока.");
         System.out.println(rsl);
-        rsl = DummyBot.answer("День добрый");
+        rsl = DummyBot.answer("2+2=?");
         System.out.println(rsl);
     }
 }
+
 
