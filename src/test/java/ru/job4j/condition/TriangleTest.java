9d0bde94b24024e43744709c76cc990246b18949
RustamSaydam <rustamsaydam@icloud.com>
2022-04-29 19:58:02 +0300

    Triangle & Triangle Test

---
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+public class TriangleTest {
+
+    @Test
+    public void when2And2And3ThenExist() {
+        double ab = 1;
+        double ac = 2;
+        double bc = 2.90;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void when3to8To2ThenExist() {
+        double ab = 3;
+        double ac = 8;
+        double bc = 2;
+        boolean result = Triangle.exist(ab, ac, bc);
+        Assert.assertFalse(result);
+    }
+}
