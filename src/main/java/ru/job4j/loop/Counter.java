2c37f87b9993f5317645a7bbda4f43f8a0a0a69f
RustamSaydam <rustamsaydam@icloud.com>
2022-05-02 18:01:11 +0300

    Counter

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -4,14 +4,14 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
-            sum = sum + 1;
+            sum = sum + i;
         }
         return sum;
     }
 
         public static void main(String[] args) {
-            System.out.println(sum(0, 5));
-            System.out.println(sum(4, 8));
+            System.out.println(sum(1, 5));
+            System.out.println(sum(3, 8));
             System.out.println(sum(1, 1));
             System.out.println(sum(20, 21));
 
