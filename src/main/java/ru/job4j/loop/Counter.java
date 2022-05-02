1461f3c66c6ff423c993160bc1adf75fc3bf7731
RustamSaydam <rustamsaydam@icloud.com>
2022-05-02 17:46:09 +0300

    Counter

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            sum = sum + 1;
+        }
+        return sum;
+    }
+
+        public static void main(String[] args) {
+            System.out.println(sum(0, 5));
+            System.out.println(sum(4, 8));
+            System.out.println(sum(1, 1));
+            System.out.println(sum(20, 21));
+
+}
+}
