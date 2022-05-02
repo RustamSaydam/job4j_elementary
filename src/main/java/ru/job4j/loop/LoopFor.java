d1e8bc508bd7216a203e978f385566f5dc9dc9b7
RustamSaydam <rustamsaydam@icloud.com>
2022-05-02 21:27:31 +0300

    LoopFor

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -1,19 +1,9 @@
 package ru.job4j.loop;
 
 public class LoopFor {
-
-    public static int sum(int start, int finish) {
-        int sum = 0;
-        for (int i = start; i <= finish; i++) {
-            sum = sum + i;
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
-        System.out.println(sum(1, 5));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-
+        for (int index = 5; index <= 10; index++) {
+            System.out.println(index);
+        }
     }
 }
