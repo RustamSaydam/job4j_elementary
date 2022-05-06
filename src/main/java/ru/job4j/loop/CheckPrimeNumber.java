642bebcfc80e14eecf2fac4d74e266dd251cbf60
RustamSaydam <rustamsaydam@icloud.com>
2022-05-06 18:59:29 +0300

    CheckPrimeNumber 12

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,7 +4,7 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         for (int i = 2; i < number; i++) {
             if ((number % i) == 0) {
-                return false;
+                break;
             }
         }
         return number > 1;
