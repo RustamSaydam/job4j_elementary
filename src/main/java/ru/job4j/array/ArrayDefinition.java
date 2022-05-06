c1c9a0a46a9fb3919b280f597c64699d5f9e9b7b
RustamSaydam <rustamsaydam@icloud.com>
2022-05-06 21:09:45 +0300

    6.0.2. Заполнение массива

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Rop Rop";
+        names[1] = "Deal 34";
+        names[2] = "RuSq";
+        names[3] = "ruRk";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 
 }
